9d283e508b7c8d87951b4b2f9075821322dc4a88
rakso20000 <rakso20000@gmail.com>
2019-10-11 23:12:07 +0200

    Disable score submission when failing in Solo mode

--- NoFailPlus/HarmonyPatch.cs
+++ NoFailPlus/HarmonyPatch.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Harmony;
+using BS_Utils.Gameplay;
 
 namespace NoFailPlus {
 	class HarmonyPatch {
 		
 		[HarmonyPrefix]
 		public static bool PreLevelFailed() {
+			
+			if (!Gamemode.IsPartyActive)
+				ScoreSubmission.DisableSubmission("NoFailPlus");
 			
 			return !Plugin.config.Value.Enable;
 			
